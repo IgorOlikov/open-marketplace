b37012a673c3725f312c159a027be4e9375af8b5
IgorOlikov <i.olikov@mail.ru>
2024-01-30 00:49:52 +0500

    comment relations

--- api/app/Models/Comment.php
+++ api/app/Models/Comment.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Comment extends Model
@@ -15,4 +17,23 @@ class Comment extends Model
     {
         return $this->morphTo();
     }
+
+    public function parentComment(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class,'parent_id','id');
+    }
+
+    public function childComments(): HasMany
+    {
+        return $this->hasMany(Comment::class,'parent_id','id');
+    }
+
+   public function owner(): BelongsTo
+   {
+       return $this->belongsTo(User::class);
+   }
+
+
+
+
 }
